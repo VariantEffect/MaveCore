926f1849141f678e2e9b7d4c38cc1bd1165b9bcd
harmatt <79935163+harmatt@users.noreply.github.com>
2022-03-03 17:06:02 -0800

    add complete docstring

--- mavecore/validation/metadata_validators.py
+++ mavecore/validation/metadata_validators.py
@@ -39,6 +39,21 @@ def validate_sra_identifier(identifier):
 
 
 def validate_keyword(kw):
+    """
+    This function validates whether or not the kw parameter is valid by
+    checking that it is a string that is not null. If kw is null
+    or is not a string, an error is raised.
+
+    Parameters
+    __________
+    kw : str
+        The keyword to be validated.
+
+    Raises
+    ______
+    ValidationError
+        If the kw argument is not a valid string.
+    """
     if is_null(kw) or not isinstance(kw, str):
         raise ValidationError(
             f"'{kw}' not a valid keyword. Keywords must be valid strings."
