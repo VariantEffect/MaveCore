73fb252481aebfdd4d5e897cc23eb6c7ee4459c1
EstelleDa <baishenlonger@live.com.sg>
2022-03-15 15:55:30 +1100

    add a setup.py and .pre-commit-config.yaml, and changed a little in __init__.py file.

---
+++ mavecore/__init__.py
@@ -0,0 +1,8 @@
+from validation.variant_validators import (
+    validate_nt_variant,
+    validate_hgvs_string,
+    validate_splice_variant,
+    validate_pro_variant,
+    validate_variant_json,
+    validate_columns_match,
+)
