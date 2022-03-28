c474564ed56f56b3210e51a26e9f64e8c8d62f86
harmatt <79935163+harmatt@users.noreply.github.com>
2022-03-28 15:57:38 -0700

    delete is_null function, import from utilities.py

--- mavecore/validation/dataset_validators.py
+++ mavecore/validation/dataset_validators.py
@@ -7,22 +7,7 @@ from numpy.testing import assert_array_equal
 from mavecore.validation import constants
 from mavecore.validation.exceptions import ValidationError
 
-def is_null(value):
-    """
-    Returns True if a stripped/lowercase value in in `nan_col_values`.
-
-    Parameters
-    __________
-    value : str
-        The value to be checked as null or not.
-
-    Returns
-    _______
-    bool
-        True value is NoneType or if value matches the stated regex patterns in constants.null_values_re.
-    """
-    value = str(value).strip().lower()
-    return constants.null_values_re.fullmatch(value) or not value
+from mavecore.validation.utilities import is_null
 
 
 class WordLimitValidator:
