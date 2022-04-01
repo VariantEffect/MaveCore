811257b37c0e1f63f6c54f9169d5666f8efd8646
harmatt <79935163+harmatt@users.noreply.github.com>
2022-03-31 17:35:25 -0700

    edit setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ if sys.version_info.major == 3 and sys.version_info.minor == 6:
 setuptools.setup(
     name="mavecore",
     version="0.1.0",
-    author="Daniel Esposito and Alan F Rubin",
+    author="MaveDB developers",
     author_email="alan.rubin@wehi.edu.au",
     description=(
         "MaveCore is to create a new dependency that contains all the shared functionality for MaveTools and MaveDB."
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/VariantEffect/MaveCore/tree/add_validation",
+    url="https://github.com/VariantEffect/MaveCore/tree/testMaveCore",
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
