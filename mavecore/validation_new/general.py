59c7731a7b3821e62e0f1eb7bc32eaf38559e7c9
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-03 11:04:57 -0700

    declare general validation and start docstrings

--- mavecore/validation_new/general.py
+++ mavecore/validation_new/general.py
@@ -1,3 +1,21 @@
+def validate_data_usage_policy(dataUsagePolicy):
+    """
+    Validates
+    :param dataUsagePolicy:
+    :return:
+    """
+def validate_license_id(licenseId):
+    """
+
+    :param licenseId:
+    :return:
+    """
+def validate_replaces_id(replacesId):
+    """
+
+    :param replacesId:
+    :return:
+    """
 def validate_processing_state(processingState):
     "creationDate": "2022-08-02",
     "modificationDate": "2022-08-02",
