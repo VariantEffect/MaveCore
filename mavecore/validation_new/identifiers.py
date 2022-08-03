400009781a0ae16e6356002db720bd279f94fc6f
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-03 13:09:13 -0700

    define and write docstrings for identifiers

--- mavecore/validation_new/identifiers.py
+++ mavecore/validation_new/identifiers.py
@@ -1,3 +1,17 @@
+def validate_doi_identifiers(doiIdentifiers):
+    """
+    This function validates a list of DOI identifiers represented as dictionaries.
+
+    Parameters:
+    __________
+    doiIdentifiers: list[dict]
+        The DOI identifiers that need to be validated
+
+    Raises:
+    ______
+    ValidationError
+        If any identifier is found to be invalid or if doiIdentifiers is not a list of dictionaries.
+    """
 "doiIdentifiers": [
       {
         "identifier": "string",
@@ -5,6 +19,22 @@
         "url": "string"
       }
     ],
+
+
+def validate_pubmed_identifiers(pubmedIdentifiers):
+    """
+    This function validates a list of PubMed identifiers represented as dictionaries.
+
+    Parameters:
+    __________
+    pubmedIdentifiers: list[dict]
+        The PubMed identifiers that need to be validated
+
+    Raises:
+    ______
+    ValidationError
+        If any identifier is found to be invalid or if pubMed Identifiers is not a list of dictionaries.
+    """
     "pubmedIdentifiers": [
       {
         "identifier": "string",
