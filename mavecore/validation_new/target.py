45179aa7edb4a76b71e416e4e145e7a0834c3a65
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-03 11:09:30 -0700

    note target gene attributes

---
+++ mavecore/validation_new/target.py
@@ -0,0 +1,26 @@
+"targetGene": {
+    "name": "string",
+    "category": "string",
+    "referenceMaps": [
+      {
+        "id": 0,
+        "genomeId": 0,
+        "targetId": 0,
+        "isPrimary": true,
+        "genome": {
+          "shortName": "string",
+          "organismName": "string",
+          "genomeId": 0,
+          "creationDate": "2022-08-02",
+          "modificationDate": "2022-08-02",
+          "id": 0
+        },
+        "creationDate": "2022-08-02",
+        "modificationDate": "2022-08-02"
+      }
+    ],
+    "wtSequence": {
+      "sequenceType": "string",
+      "sequence": "string"
+    }
+  },
