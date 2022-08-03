20c9d6bdf0d3570e1c9013d345846072c3cd4b48
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-03 11:06:02 -0700

    declare metadata validation and start docstring, mark function as TODO

---
+++ mavecore/validation_new/metadata.py
@@ -0,0 +1,15 @@
+def validate_metadata(extraMetadata):
+    """
+    This function validates metadata associated with an upload.
+
+    Parameters:
+    __________
+    extraMetadata: Dict
+        The metadata to be validated.
+
+    Raises:
+    ______
+    ValidationError
+        If any of the key:value pairs are not valid.
+    """
+    #TODO
