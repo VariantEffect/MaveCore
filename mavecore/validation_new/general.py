c653118ded3137636b924fc10089aabc05527b87
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-03 13:09:30 -0700

    reformat

--- mavecore/validation_new/general.py
+++ mavecore/validation_new/general.py
@@ -8,18 +8,24 @@ def validate_data_usage_policy(dataUsagePolicy):
     :param dataUsagePolicy:
     :return:
     """
+
+
 def validate_license_id(licenseId):
     """
 
     :param licenseId:
     :return:
     """
+
+
 def validate_replaces_id(replacesId):
     """
 
     :param replacesId:
     :return:
     """
+
+
 def validate_processing_state(processingState):
     "creationDate": "2022-08-02",
     "modificationDate": "2022-08-02",
