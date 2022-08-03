117694a2433e478ecfd176c39da984137fa02d35
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-03 11:08:04 -0700

    write docstring and validation code to validate short description

--- mavecore/validation_new/summary.py
+++ mavecore/validation_new/summary.py
@@ -18,6 +18,24 @@ def validate_title(title):
     # check that title is not too long
 
 def validate_short_description(shortDescription):
+    """
+    Validates the short description of an experiment set, an experiment, or a scoreset.
+
+    Parameters:
+    __________
+    shortDescription: str
+        The short description to be validated.
+
+    Raises:
+    ______
+    ValidationError if the short description is too long or is not a string.
+    """
+    # check if short description is a string
+    if type(shortDescription) != str: raise ValidationError("The short description must be a string.")
+
+    # check if short description is too long
+    count = len(shortDescription.split(" "))
+    if count > 50: raise ValidationError("The short description must be less than or equal to 50 words.")
 
 def validate_abstract(abstractText):
     """
