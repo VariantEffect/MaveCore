d4dc54046e87d23a31e35215dc934a5ba020accb
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-03 11:37:23 -0700

    note all attributes of an experiment

--- mavecore/validation_new/experiment.py
+++ mavecore/validation_new/experiment.py
@@ -58,3 +58,48 @@ def validate_experiemnt(experiment):
     "processingState": "string"
   }
 ]
+
+    #  "experiment": {
+    #    "title": "string",
+    #    "shortDescription": "string",
+    #    "abstractText": "string",
+    #    "methodText": "string",
+    #    "extraMetadata": {},
+    #    "keywords": [
+    #      "string"
+    #    ],
+    #    "urn": "string",
+    #    "numScoresets": 0,
+    #    "createdBy": {
+    #      "orcid_id": "string",
+    #      "firstName": "string",
+    #      "lastName": "string",
+    #      "email": "string"
+    #    },
+    #    "modifiedBy": {
+    #      "orcid_id": "string",
+    #      "firstName": "string",
+    #      "lastName": "string",
+    #      "email": "string"
+    #    },
+    #    "creationDate": "2022-08-02",
+    #    "modificationDate": "2022-08-02",
+    #    "publishedDate": "2022-08-02",
+    #    "experimentSetUrn": "string",
+    #    "doiIdentifiers": [
+    #      {
+    #        "identifier": "string",
+    #        "id": 0,
+    #        "url": "string"
+    #      }
+    #    ],
+    #    "pubmedIdentifiers": [
+    #      {
+    #        "identifier": "string",
+    #        "id": 0,
+    #        "url": "string",
+    #        "referenceHtml": "string"
+    #      }
+    #    ],
+    #    "processingState": "string"
+    #  },
