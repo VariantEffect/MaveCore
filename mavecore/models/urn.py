28d206be13341a86831b7edb7e95786edb32612b
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-11 11:58:09 -0700

    add urn specific validation

--- mavecore/models/urn.py
+++ mavecore/models/urn.py
@@ -7,7 +7,23 @@ from ..validation_new.exceptions import ValidationError
 class Urn(BaseModel):
     urn: str
 
+
+class ExperimentUrn(Urn):
     @validator('urn')
     def must_match_regular_expression(cls, v):
-        if not MAVEDB_ANY_URN_RE.match(v):
-            raise ValueError("{}'s is not a valid urn.".format(v))
+        if not (MAVEDB_EXPERIMENTSET_URN_RE.match(v) or MAVEDB_TMP_URN_RE.match(v)):
+            raise ValidationError("{}'s is not a valid Experiment Set urn.".format(v))
+
+
+class ExperimentSetUrn(Urn):
+    @validator('urn')
+    def must_match_regular_expression(cls, v):
+        if not (MAVEDB_EXPERIMENT_URN_RE.match(v) or MAVEDB_TMP_URN_RE.match(v)):
+            raise ValidationError("{}'s is not a valid Experiment urn.".format(v))
+
+
+class ScoreSetUrn(Urn):
+    @validator('urn')
+    def must_match_regular_expression(cls, v):
+        if not (MAVEDB_SCORESET_URN_RE.match(v) or MAVEDB_TMP_URN_RE.match(v)):
+            raise ValidationError("{}'s is not a valid score set urn.".format(v))
