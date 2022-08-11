8292a0d53110dff147f1c87a231b2128ca59e3ef
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-11 13:46:45 -0700

    write unittests for user model validation

---
+++ tests/test_pydantic_validation/user.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from mavecore.models.user import User
+
+
+class Test(TestCase):
+    def test_valid(self):
+        user = {
+          "orcid_id": "idididid",
+          "firstName": "first",
+          "lastName": "last",
+          "email": "firstlast@email.edu",
+        }
+        User.parse_obj(user)
+
+    def test_invalid_email(self):
+        user = {
+            "orcid_id": "idididid",
+            "firstName": "first",
+            "lastName": "last",
+            "email": "firstlastemail.edu",
+        }
+        with self.assertRaises(ValueError):
+            User.parse_obj(user)
