fd5aba88c7dd6799e71b27fc48fe1cac52c4b817
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-10 17:08:04 -0700

    add validation to urn model

--- mavecore/models/urn.py
+++ mavecore/models/urn.py
@@ -4,3 +4,9 @@ from ..validation_new.constants.urn import *
 
 
 class Urn(BaseModel):
+    urn: str
+
+    @validator('urn')
+    def must_match_regular_expression(cls, v):
+        if not MAVEDB_ANY_URN_RE.match(v):
+            raise ValueError("{}'s is not a valid urn.".format(v))
