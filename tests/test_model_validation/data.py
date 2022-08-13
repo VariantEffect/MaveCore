6ebb1bebce1d40c0aaaea156a70dbf25656c2bdd
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-12 18:47:16 -0700

    test Experiment model exclude optional attributes

--- tests/test_model_validation/data.py
+++ tests/test_model_validation/data.py
@@ -89,4 +89,73 @@ class TestExperiment(TestCase):
             "pubmedIdentifiers": [pubmed_identifier],
             "processingState": "string",
         }
-        DataSet.parse_obj(dictionary)
+        Experiment.parse_obj(experiment)
+
+    def test_valid_exclude_optional(self):
+        experiment = {
+            "title": "title",
+            "shortDescription": "short description",
+            "abstractText": "abstract",
+            "methodText": "methods",
+        }
+        Experiment.parse_obj(experiment)
+
+
+class TestExperimentSet(TestCase):
+    def test_valid(self):
+        user = {"orcid_id": "id", "firstName": "first", "lastName": "last", "email": "firstlast@email.edu"}
+        experiment = {"title": "title", "shortDescription": "short description", "abstractText": "abstract", "methodText": "methods"}
+        experimentset = {
+            "title": "title",
+            "shortDescription": "short description",
+            "abstractText": "abstract",
+            "methodText": "methods",
+            "extraMetadata": {},
+            "creationDate": "2022-02-02",
+            "publishedDate": "2022-02-02",
+            "modificationDate": "2022-02-02",
+            "createdBy": user,
+            "modifiedBy": user,
+            #"urn": "urn",
+            "id": 0,
+            "experiments": [experiment],
+            "numExperiments": 1,
+        }
+        ExperimentSet.parse_obj(experimentset)
+
+
+class TestScoreSet(TestCase):
+    def test_valid_all_fields(self):
+        user = {"orcid_id": "id", "firstName": "first", "lastName": "last", "email": "firstlast@email.edu"}
+        experiment = {"title": "title", "shortDescription": "short description", "abstractText": "abstract", "methodText": "methods"}
+        doi_identifier = {"identifier": "id"}
+        pubmed_identifier = {"identifier": "id"}
+        genome = {"shortName": "name", "organismName": "organism", "genomeId": 0, "id": 0}
+        reference_map = {"id": 0, "genomeId": 0, "targetId": 0, "isPrimary": True, "genome": genome}
+        sequence = {"sequenceType": "DNA", "sequence": "ATCG"}
+        target = {"name": "name", "category": "Protein coding", "referenceMaps": [reference_map], "wtSequence": sequence,}
+        scoreset = {
+            "title": "title",
+            "shortDescription": "short description",
+            "abstractText": "abstract",
+            "methodText": "methods",
+            "extraMetadata": {},
+            "creationDate": "2022-02-02",
+            "publishedDate": "2022-02-02",
+            "modificationDate": "2022-02-02",
+            "createdBy": user,
+            "modifiedBy": user,
+            #"urn": "urn",
+            "dataUsagePolicy": "policy",
+            "licenceId": 0,
+            "replacesId": 0,
+            "keywords": ["string"],
+            "numVariants": 0,
+            "experiment": experiment,
+            "doiIdentifiers": [doi_identifier],
+            "pubmedIdentifiers": [pubmed_identifier],
+            "targetGene": target,
+            "datasetColumns": {},
+            "private": True,
+        }
+        ScoreSet.parse_obj(scoreset)
