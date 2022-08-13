480c6303148391d536a45e93b58456461d1decb8
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-12 18:45:33 -0700

    test DataSet model

--- tests/test_model_validation/data.py
+++ tests/test_model_validation/data.py
@@ -3,6 +3,22 @@ from pydantic import ValidationError
 from mavecore.models.data import DataSet, Experiment, ExperimentSet, ScoreSet
 
 
+class TestDataSet(TestCase):
+    def test_valid_all_fields(self):
+        user = {"orcid_id": "id", "firstName": "first", "lastName": "last", "email": "firstlast@email.edu"}
+        dataset = {
+            "title": "title",
+            "shortDescription": "short description",
+            "abstractText": "abstract",
+            "methodText": "methods",
+            "extraMetadata": {},
+            "creationDate": "2022-02-02",
+            "publishedDate": "2022-02-02",
+            "modificationDate": "2022-02-02",
+            "createdBy": user,
+            "modifiedBy": user,
+        }
+        DataSet.parse_obj(dataset)
 
 class Test(TestCase):
     def test_no_change(self):
