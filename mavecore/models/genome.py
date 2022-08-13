84046847dcac34180d2f68ce8301fcf817c6fcb5
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-12 18:47:52 -0700

    validate date

--- mavecore/models/genome.py
+++ mavecore/models/genome.py
@@ -10,3 +10,10 @@ class Genome(BaseModel):
     creationDate: Optional[datetime]
     modificationDate: Optional[datetime]
     id: int
+
+    @validator('creationDate', 'modificationDate')
+    def date_must_match_regex(cls, v):
+        # regular expression for validating a date
+        regex = '%Y-%m-%d'
+        if not bool(datetime.strptime(v, regex)):
+            raise ValidationError("{}'s is not a valid date.".format(v))
