690890856f0ddaeaa5c2f093a84207107128092c
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-12 18:49:03 -0700

    mark attributes as optional, validate dates

--- mavecore/models/map.py
+++ mavecore/models/map.py
@@ -11,5 +11,12 @@ class ReferenceMap(BaseModel):
     targetId: int
     isPrimary: bool
     genome: Genome
-    creationDate: datetime
-    modificationDate: Optional[datetime]
+    creationDate: Optional[str]
+    modificationDate: Optional[str]
+
+    @validator('creationDate', 'modificationDate')
+    def date_must_match_regex(cls, v):
+        # regular expression for validating a date
+        regex = '%Y-%m-%d'
+        if not bool(datetime.strptime(v, regex)):
+            raise ValidationError("{}'s is not a valid date.".format(v))
