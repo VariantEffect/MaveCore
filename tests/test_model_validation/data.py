2613ded458d57b26f695e0f49f43ae14ad4b8787
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-12 18:46:52 -0700

    test DataSet model invalid dates

--- tests/test_model_validation/data.py
+++ tests/test_model_validation/data.py
@@ -29,10 +29,64 @@ class TestDataSet(TestCase):
         }
         DataSet.parse_obj(dataset)
 
-        dictionary ={
-          "title": "string",
-          "shortDescription": "string",
-          "abstractText": "string",
-          "methodText": "string",
+    def test_invalid_creation_date(self):
+        dataset = {
+            "title": "title",
+            "shortDescription": "short description",
+            "abstractText": "abstract",
+            "methodText": "methods",
+            "creationDate": "2022-02-02-",
+        }
+        with self.assertRaises(ValidationError):
+            DataSet.parse_obj(dataset)
+
+    def test_invalid_published_date(self):
+        dataset = {
+            "title": "title",
+            "shortDescription": "short description",
+            "abstractText": "abstract",
+            "methodText": "methods",
+            "publishedDate": "2022-02-02-",
+        }
+        with self.assertRaises(ValidationError):
+            DataSet.parse_obj(dataset)
+
+    def test_invalid_modification_date(self):
+        dataset = {
+            "title": "title",
+            "shortDescription": "short description",
+            "abstractText": "abstract",
+            "methodText": "methods",
+            "creationDate": "2022-02-02",
+            "publishedDate": "2022-02-02",
+            "modificationDate": "a",
+        }
+        with self.assertRaises(ValidationError):
+            DataSet.parse_obj(dataset)
+
+
+class TestExperiment(TestCase):
+    def test_valid_all_fields(self):
+        user = {"orcid_id": "id", "firstName": "first", "lastName": "last", "email": "firstlast@email.edu"}
+        doi_identifier = {"identifier": "id"}
+        pubmed_identifier = {"identifier": "id"}
+        experiment = {
+            "title": "title",
+            "shortDescription": "short description",
+            "abstractText": "abstract",
+            "methodText": "methods",
+            "extraMetadata": {},
+            "creationDate": "2022-02-02",
+            "publishedDate": "2022-02-02",
+            "modificationDate": "2022-02-02",
+            "createdBy": user,
+            "modifiedBy": user,
+            "urn": "tmp:070b3886-ed72-4ce9-a574-6754ad00310b",
+            "keywords": ["string"],
+            "numScoresets": 0,
+            #"experimentSetUrn": "urn",
+            "doiIdentifiers": [doi_identifier],
+            "pubmedIdentifiers": [pubmed_identifier],
+            "processingState": "string",
         }
         DataSet.parse_obj(dictionary)
