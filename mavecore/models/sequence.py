b9a63acae6bbfa60decce6c95dcf68cc045eb1c8
harmatt <79935163+harmatt@users.noreply.github.com>
2022-08-12 18:49:44 -0700

    validate sequence type category

--- mavecore/models/sequence.py
+++ mavecore/models/sequence.py
@@ -4,3 +4,10 @@ from pydantic import BaseModel, ValidationError, validator
 class WildType(BaseModel):
     sequenceType: str
     sequence: str
+
+    @validator('sequenceType')
+    def validate_category(cls, v):
+        valid_sequence_types = ["Infer", "DNA", "Protein"]
+        if v not in valid_sequence_types:
+            raise ValidationError("{}'s is not a valid sequence type. Valid sequence types are "
+                                  "Infer, DNA, and Protein".format(v))
