69347d20c024f911c53851885289f5a55089c87b
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-14 14:34:50 -0700

    refactor unittests to work with setUp methods

--- tests/test_validation/dataframe.py
+++ tests/test_validation/dataframe.py
@@ -15,36 +15,17 @@ class TestValidateNoNullColumnsOrRows(TestCase):
         )
 
     def test_valid(self):
-        dataframe = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G"],
-                general.hgvs_pro_column: ["p.Leu5Glu"],
-                general.hgvs_splice_column: ["c.1A>G"],
-            }
-        )
-        validate_no_null_columns_or_rows(dataframe)
+        validate_no_null_columns_or_rows(self.dataframe)
 
     def test_null_row(self):
-        dataframe = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G", None],
-                general.hgvs_pro_column: ["p.Leu5Glu", None],
-                general.hgvs_splice_column: ["c.1A>G", None],
-            }
-        )
+        self.dataframe.loc[1] = [None, None, None]
         with self.assertRaises(AssertionError):
-            validate_no_null_columns_or_rows(dataframe)
+            validate_no_null_columns_or_rows(self.dataframe)
 
     def test_null_column(self):
-        dataframe = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G", None],
-                general.hgvs_pro_column: [None, None],
-                general.hgvs_splice_column: ["c.1A>G", None],
-            }
-        )
+        self.dataframe[hgvs_pro_column][0] = None
         with self.assertRaises(AssertionError):
-            validate_no_null_columns_or_rows(dataframe)
+            validate_no_null_columns_or_rows(self.dataframe)
 
 
 class TestValidateColumnNames(TestCase):
@@ -59,55 +40,27 @@ class TestValidateColumnNames(TestCase):
         )
 
     def test_valid_column_names(self):
-        dataframe = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G"],
-                general.hgvs_pro_column: ["p.Leu5Glu"],
-                general.hgvs_splice_column: ["c.1A>G"],
-                "scores": [1.000],
-            }
-        )
-        validate_column_names(dataframe.columns)
+        validate_column_names(self.dataframe.columns)
 
     def test_missing_hgvs_column(self):
-        dataframe = pd.DataFrame(
-            {
-                "scores": [1.000],
-            }
-        )
+        self.dataframe = self.dataframe.drop([hgvs_nt_column, hgvs_pro_column, hgvs_splice_column], axis=1)
         with self.assertRaises(ValidationError):
-            validate_column_names(dataframe.columns)
+            validate_column_names(self.dataframe.columns)
 
     def test_hgvs_in_wrong_location(self):
-        dataframe = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G"],
-                "scores": [1.000],
-                general.hgvs_splice_column: ["c.1A>G"],
-            }
-        )
+        self.dataframe = self.dataframe[[hgvs_nt_column, required_score_column, hgvs_pro_column, hgvs_splice_column]]
         with self.assertRaises(ValidationError):
-            validate_column_names(dataframe.columns)
+            validate_column_names(self.dataframe.columns)
 
     def test_no_additional_columns_beyond_hgvs(self):
-        dataframe = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G"],
-            }
-        )
+        self.dataframe = self.dataframe.drop([hgvs_pro_column, hgvs_splice_column, required_score_column], axis=1)
         with self.assertRaises(ValidationError):
-            validate_column_names(dataframe.columns)
+            validate_column_names(self.dataframe.columns)
 
     def test_null_column_name(self):
-        dataframe = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G"],
-                "null": ["c.1A>G"],
-                "scores": [1.000],
-            }
-        )
+        self.dataframe.rename(columns={hgvs_splice_column: 'null'}, inplace=True)
         with self.assertRaises(ValidationError):
-            validate_column_names(dataframe.columns)
+            validate_column_names(self.dataframe.columns)
 
 
 class TestValidateVariants(TestCase):
@@ -121,14 +74,7 @@ class TestValidateVariants(TestCase):
         )
 
     def test_valid_variants(self):
-        dataframe = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G", "c.1A>G", "c.1A>G"],
-                general.hgvs_pro_column: ["p.Leu5Glu", "p.Leu5Glu", "p.Leu5Glu"],
-                general.hgvs_splice_column: ["c.1A>G", "c.1A>G", "c.1A>G"],
-            }
-        )
-        validate_variants(dataframe["hgvs_nt"])
+        validate_variants(self.dataframe["hgvs_nt"])
 
     def test_invalid_variants(self):
         pass
@@ -165,72 +111,19 @@ class TestDataframesDefineSameVariants(TestCase):
         )
 
     def test_valid(self):
-        scores = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G"],
-                general.hgvs_pro_column: ["p.Leu5Glu"],
-                general.hgvs_splice_column: ["c.1A>G"],
-            }
-        )
-        counts = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G"],
-                general.hgvs_pro_column: ["p.Leu5Glu"],
-                general.hgvs_splice_column: ["c.1A>G"],
-            }
-        )
-        validate_dataframes_define_same_variants(scores, counts)
+        validate_dataframes_define_same_variants(self.scores, self.counts)
 
     def test_counts_defines_different_nt_variants(self):
-        scores = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.1A>G"],
-                general.hgvs_pro_column: [None],
-                general.hgvs_splice_column: [None],
-            }
-        )
-        counts = pd.DataFrame(
-            {
-                general.hgvs_nt_column: ["c.2A>G"],
-                general.hgvs_pro_column: [None],
-                general.hgvs_splice_column: [None],
-            }
-        )
+        self.counts[hgvs_nt_column][0] = "c.2A>G"
         with self.assertRaises(ValidationError):
-            validate_dataframes_define_same_variants(scores, counts)
+            validate_dataframes_define_same_variants(self.scores, self.counts)
 
     def test_counts_defines_different_splice_variants(self):
-        scores = pd.DataFrame(
-            {
-                general.hgvs_nt_column: [None],
-                general.hgvs_splice_column: ["c.1A>G"],
-                general.hgvs_pro_column: [None],
-            }
-        )
-        counts = pd.DataFrame(
-            {
-                general.hgvs_nt_column: [None],
-                general.hgvs_splice_column: ["c.2A>G"],
-                general.hgvs_pro_column: [None],
-            }
-        )
+        self.counts[hgvs_splice_column][0] = "c.2A>G"
         with self.assertRaises(ValidationError):
-            validate_dataframes_define_same_variants(scores, counts)
+            validate_dataframes_define_same_variants(self.scores, self.counts)
 
     def test_counts_defines_different_pro_variants(self):
-        scores = pd.DataFrame(
-            {
-                general.hgvs_nt_column: [None],
-                general.hgvs_splice_column: [None],
-                general.hgvs_pro_column: ["p.Leu5Glu"],
-            }
-        )
-        counts = pd.DataFrame(
-            {
-                general.hgvs_nt_column: [None],
-                general.hgvs_splice_column: [None],
-                general.hgvs_pro_column: ["p.Leu75Glu"],
-            }
-        )
+        self.counts[hgvs_pro_column][0] = "p.Leu75Glu"
         with self.assertRaises(ValidationError):
-            validate_dataframes_define_same_variants(scores, counts)
+            validate_dataframes_define_same_variants(self.scores, self.counts)
