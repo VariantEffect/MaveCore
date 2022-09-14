f0b8f07b3b5c590834e85ddba80954c3b627a230
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-14 13:38:03 -0700

    validate all

---
+++ mavecore/validation/validate.py
@@ -0,0 +1,32 @@
+from mavecore.models.data import Experiment, ScoreSet
+from mavecore.validation.dataframe import validate_dataframes
+
+
+def validate(dataset, dataset_type, scores=None, counts=None):
+    """
+    This function validates data to by uploaded to MaveDB. Descriptive errors will be raised if any of the validation
+    fails. Scores and counts are optional as this function accepts both experiments and scoresets.
+
+    Parameters
+    __________
+    dataset: dict
+        The scoreset or experiment to be uploaded. This will be cast into a pydantic object.
+    dataset_type: str
+        The type of dataset that the first argument is, either "experiments" or "scoresets".
+    scores: Pandas.DataFrame
+        The scores dataframe as a Pandas DataFrame.
+    counts: Pandas.DataFrame
+        The counts dataframe as a Pandas DataFrame.
+
+    Raises
+    ______
+    ValueError
+        If the dataset_type attribute is not a string that reads `experiments` or `scoresets`.
+    """
+    if dataset_type == "experiments":
+        Experiment.parse_obj(dataset)
+    elif dataset_type == "scoresets":
+        ScoreSet.parse_obj(dataset)
+        validate_dataframes(scores=scores, counts=counts)
+    else:
+        raise ValueError("The dataset_type must be a string that reads `experiments` or `scoresets`.")
