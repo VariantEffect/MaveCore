72dd3b61927cea4f56719a3a854dee829c28947f
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-14 17:00:51 -0700

    refactor pydantic unittests to work with setUp methods

--- tests/models/identifier.py
+++ tests/models/identifier.py
@@ -12,27 +12,17 @@ class TestIdentifier(TestCase):
         }
 
     def test_valid_all_fields(self):
-        identifier = {
-            "identifier": "10.1038/s41588-018-0122-z",
-            "id": 0,
-            "url": "https://www.uw.edu",
-        }
-        Identifier.parse_obj(identifier)
+        Identifier.parse_obj(self.identifier)
 
     def test_valid_exclude_optional(self):
-        identifier = {
-            "identifier": "29785012",
-        }
-        Identifier.parse_obj(identifier)
+        self.identifier.pop("id")
+        self.identifier.pop("url")
+        Identifier.parse_obj(self.identifier)
 
     def test_invalid_url(self):
-        identifier = {
-            "identifier": "29785012",
-            "id": 0,
-            "url": "www.uw.edu",
-        }
+        self.identifier["url"] = "www.uw.edu"
         with self.assertRaises(ValidationError):
-            Identifier.parse_obj(identifier)
+            Identifier.parse_obj(self.identifier)
 
 
 class TestDoiIdentifier(TestCase):
@@ -44,19 +34,12 @@ class TestDoiIdentifier(TestCase):
         }
 
     def test_valid_all_fields(self):
-        doi_identifier = {
-            "identifier": "10.1038/s41588-018-0122-z",
-            "id": 0,
-            "url": "https://www.uw.edu",
-        }
-        DoiIdentifier.parse_obj(doi_identifier)
+        DoiIdentifier.parse_obj(self.doi_identifier)
 
     def test_invalid_type_of_identifier(self):
-        identifier = {
-            "identifier": "29785012",
-        }
+        self.doi_identifier["identifier"] = "29785012"
         with self.assertRaises(ValidationError):
-            DoiIdentifier.parse_obj(identifier)
+            DoiIdentifier.parse_obj(self.doi_identifier)
 
 
 class TestPubmedIdentifier(TestCase):
@@ -69,23 +52,15 @@ class TestPubmedIdentifier(TestCase):
         }
 
     def test_valid_all_fields(self):
-        pubmed_identifier = {
-            "identifier": "29785012",
-            "id": 0,
-            "url": "https://www.uw.edu",
-            "referenceHtml": "referencehtml",
-        }
-        PubmedIdentifier.parse_obj(pubmed_identifier)
+        PubmedIdentifier.parse_obj(self.pubmed_identifier)
 
     def test_valid_exclude_optional(self):
-        pubmed_identifier = {
-            "identifier": "29785012",
-        }
-        PubmedIdentifier.parse_obj(pubmed_identifier)
+        self.pubmed_identifier.pop("id")
+        self.pubmed_identifier.pop("url")
+        self.pubmed_identifier.pop("referenceHtml")
+        PubmedIdentifier.parse_obj(self.pubmed_identifier)
 
     def test_invalid_type_of_identifier(self):
-        identifier = {
-            "identifier": "10.1038/s41588-018-0122-z",
-        }
+        self.pubmed_identifier["identifier"] = "10.1038/s41588-018-0122-z"
         with self.assertRaises(ValidationError):
-            PubmedIdentifier.parse_obj(identifier)
+            PubmedIdentifier.parse_obj(self.pubmed_identifier)
