7a07ff2180230448911cf1948ed6f398b623b2e3
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-16 09:27:31 -0700

    add setUp methods to test classes

--- tests/models/target.py
+++ tests/models/target.py
@@ -4,6 +4,19 @@ from mavecore.models.target import TargetGene
 
 
 class TestTargetGene(TestCase):
+    def setUp(self):
+        reference_map = {"genomeId": 0, "targetId": 0}
+        sequence = {"sequenceType": "Protein", "sequence": "ATCG"}
+        self.target = {
+            "name": "name",
+            "category": "Protein coding",
+            "ensembleIdId": 0,
+            "refseqIdId": 0,
+            "uniprotIdId": 0,
+            "referenceMaps": [reference_map],
+            "wtSequence": sequence,
+        }
+
     def test_valid_all_fields(self):
         genome = {"shortName": "name", "organismName": "organism", "genomeId": 0, "id": 0}
         reference_map = {"id": 0, "genomeId": 0, "targetId": 0, "isPrimary": True, "genome": genome}
