1a0740035702c795757e5c0332fbf6653c6be0ca
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-15 19:28:19 -0700

    remove keyword test cases

--- tests/validation/identifier.py
+++ tests/validation/identifier.py
@@ -130,24 +130,6 @@ class TestUniprotValidators(TestCase):
         with self.assertRaises(ValidationError):
             validate_uniprot_identifier("P123")
 
-    def test_ve_invalid_uniprot_list(self):
-        with self.assertRaises(ValidationError):
-            validate_keyword_list(["protein", 555])
-
     def test_passes_valid_uniprot_id(self):
         validate_uniprot_identifier("P01133")
 
-
-class TestKeywordValidators(TestCase):
-    """
-    Tests that each validator throws the appropriate :class:`ValidationError`
-    when passed invalid input.
-    """
-
-    def test_ve_invalid_keyword(self):
-        with self.assertRaises(ValidationError):
-            validate_keyword(555)
-
-    def test_ve_invalid_keyword_in_list(self):
-        with self.assertRaises(ValidationError):
-            validate_keyword_list(["protein", 555])
