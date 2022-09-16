e211595810ffd668017a2340f3d3da24fac709a8
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-15 19:27:43 -0700

    remove keyword validation

--- mavecore/validation/identifier.py
+++ mavecore/validation/identifier.py
@@ -31,28 +31,6 @@ def validate_sra_identifier(identifier):
         )
 
 
-def validate_keyword(kw):
-    """
-    This function validates whether or not the kw parameter is valid by
-    checking that it is a string that is not null. If kw is null
-    or is not a string, an error is raised.
-
-    Parameters
-    __________
-    kw : str
-        The keyword to be validated.
-
-    Raises
-    ______
-    ValidationError
-        If the kw argument is not a valid string.
-    """
-    if is_null(kw) or not isinstance(kw, str):
-        raise ValidationError(
-            f"'{kw}' not a valid keyword. Keywords must be valid strings."
-        )
-
-
 def validate_pubmed_identifier(identifier):
     """
     Validates whether the identifier is a valid PubMed identifier.
@@ -161,22 +139,6 @@ def validate_genome_identifier(identifier):
         raise ValidationError(
             f"'{identifier}' is not a valid GenBank or RefSeq genome assembly."
         )
-
-
-def validate_keyword_list(values):
-    """
-    This function takes a list of keyword values and validates that each one is valid.
-    A valid keyword is a non-null string. The validate_keyword function will raise an
-    ValidationError if any of the keywords are invalid.
-
-    Parameters
-    __________
-    values : list[str]
-        The list of values to be validated.
-    """
-    for value in values:
-        if not is_null(value):
-            validate_keyword(value)
 
 
 def validate_pubmed_list(values):
