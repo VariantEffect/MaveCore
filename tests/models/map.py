8529a56acff6052067bfe62e9da6311d6b5e258f
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-16 09:29:14 -0700

    delete unneeded test cases

--- tests/models/map.py
+++ tests/models/map.py
@@ -11,27 +11,3 @@ class TestReferenceMap(TestCase):
 
     def test_valid_all_fields(self):
         ReferenceMap.parse_obj(self.reference_map)
-
-    def test_valid_exclude_optional(self):
-        genome = {"shortName": "name", "organismName": "organism", "genomeId": 0, "id": 0}
-        reference_map = {
-            "id": 0,
-            "genomeId": 0,
-            "targetId": 0,
-            "isPrimary": True,
-            "genome": genome,
-        }
-        ReferenceMap.parse_obj(reference_map)
-
-    def test_invalid_creation_date(self):
-        genome = {"shortName": "name", "organismName": "organism", "genomeId": 0, "id": 0}
-        reference_map = {
-            "id": 0,
-            "genomeId": 0,
-            "targetId": 0,
-            "isPrimary": True,
-            "genome": genome,
-            "creationDate": "2022-02-02-",
-        }
-        with self.assertRaises(ValidationError):
-            ReferenceMap.parse_obj(reference_map)
