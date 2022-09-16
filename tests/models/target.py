8a62511a51a1d79c6be722885e374d31073a38d5
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-16 09:28:21 -0700

    refactor test cases to work with setUp methods

--- tests/models/target.py
+++ tests/models/target.py
@@ -18,37 +18,14 @@ class TestTargetGene(TestCase):
         }
 
     def test_valid_all_fields(self):
-        genome = {"shortName": "name", "organismName": "organism", "genomeId": 0, "id": 0}
-        reference_map = {"id": 0, "genomeId": 0, "targetId": 0, "isPrimary": True, "genome": genome}
-        sequence = {"sequenceType": "type", "sequence": "ATCG"}
-        target = {
-            "name": "name",
-            "category": "Protein coding",
-            "referenceMaps": [reference_map],
-            "wtSequence": sequence,
-        }
-        TargetGene.parse_obj(target)
+        TargetGene.parse_obj(self.target)
 
     def test_invalid_category(self):
-        genome = {"shortName": "names", "organismName": "organism", "genomeId": 0, "id": 0}
-        reference_map = {"id": 0, "genomeId": 0, "targetId": 0, "isPrimary": True, "genome": genome}
-        sequence = {"sequenceType": "type", "sequence": "ATCG"}
-        target = {
-            "name": "name",
-            "category": "Protein",
-            "referenceMaps": [reference_map],
-            "wtSequence": sequence,
-        }
+        self.target["category"] = "Protein"
         with self.assertRaises(ValidationError):
-            TargetGene.parse_obj(target)
+            TargetGene.parse_obj(self.target)
 
     def test_invalid_missing_required_field(self):
-        genome = {"shortName": "name", "organismName": "organism", "genomeId": 0, "id": 0}
-        reference_map = {"id": 0, "genomeId": 0, "targetId": 0, "isPrimary": True, "genome": genome}
-        target = {
-            "name": "name",
-            "category": "Protein coding",
-            "referenceMaps": [reference_map],
-        }
+        self.target.pop("wtSequence")
         with self.assertRaises(ValidationError):
-            TargetGene.parse_obj(target)
+            TargetGene.parse_obj(self.target)
