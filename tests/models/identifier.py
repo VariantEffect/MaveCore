6e24bf0ac425b8c64ecf1f9923a79575543d566c
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-23 13:16:47 -0700

    test ExternalIdentifier

--- tests/models/identifier.py
+++ tests/models/identifier.py
@@ -46,3 +46,16 @@ class TestPubmedIdentifier(TestCase):
         self.pubmed_identifier["identifier"] = "10.1038/s41588-018-0122-z"
         with self.assertRaises(ValidationError):
             PubmedIdentifier.parse_obj(self.pubmed_identifier)
+
+
+class TestExternalIdentifier(TestCase):
+    def setUp(self):
+        self.external_identifier_id = {"dbname": "uniprot", "identifier": "P01133"}
+        self.external_identifier = {"identifier": self.external_identifier_id, "offset": 0}
+
+    """def test_valid_external_identifier_id(self):
+        print(hasattr(self.external_identifier_id, "dbname"))
+        ExternalIdentifierId.parse_obj(self.external_identifier_id)"""
+
+    def test_valid_external_identifier(self):
+        ExternalIdentifier.parse_obj(self.external_identifier)
