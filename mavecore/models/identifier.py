87396b26f76c87808e7f9621833212580449f9dc
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-23 13:15:02 -0700

    comment out ExternalIdentifierId

--- mavecore/models/identifier.py
+++ mavecore/models/identifier.py
@@ -21,9 +21,30 @@ class PubmedIdentifier(Identifier):
         id.validate_pubmed_identifier(v)
 
 
-class ExternalIdentifierId(BaseModel):
+'''class ExternalIdentifierId(BaseModel):
     dbname: str
     identifier: str
+
+    @root_validator(pre=True)
+    def check_passwords_match(cls, values):
+        print(values.get("dbname"))
+        # TODO resolve errors when using root_validator
+        #TODO confirm what valid dbname(s) are
+        #dbname, dbid = values.get('dbname'), values.get('identifier')
+        #print(dbname)
+        #print(dbid)
+        #if dbname == "sra":
+        #    identifier.validate_sra_identifier(dbid)
+        #elif dbname == "ensembl":
+        #    identifier.validate_ensembl_identifier(dbid)
+        #elif dbname == "uniprot":
+        #    identifier.validate_uniprot_identifier(dbid)
+        #elif dbname == "refseq":
+        #    identifier.validate_refseq_identifier(dbid)
+        #elif dbname == "genome":
+        #    identifier.validate_genome_identifier(dbid)
+        #else:
+        #    raise ValidationError("dbname must be valid dbname from this list: ")'''
 
 
 class ExternalIdentifier(BaseModel):
