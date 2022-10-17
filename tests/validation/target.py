fcb9b1d36d338bbe1ae64ae0d8dec704ba6f47bf
harmatt <79935163+harmatt@users.noreply.github.com>
2022-10-17 16:35:57 -0400

    outline unittest for target

---
+++ tests/validation/target.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from mavecore.validation.target import *
+
+
+class TestValidateTargetCategory(TestCase):
+    def test_valid(self):
+        pass
+
+
+class TestValidateSequenceCategory(TestCase):
+    def test_valid(self):
+        pass
+
+
+class TestValidateTargetSequence(TestCase):
+    def test_valid(self):
+        pass
