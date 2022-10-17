44d34002baa955dd268b5394b428fa8636e5ee64
harmatt <79935163+harmatt@users.noreply.github.com>
2022-10-17 16:40:27 -0400

    validate pydantic attribute

--- mavecore/models/sequence.py
+++ mavecore/models/sequence.py
@@ -10,3 +10,7 @@ class WildType(BaseModel):
     @validator('sequenceType')
     def validate_category(cls, v):
         target.validate_sequence_category(v)
+
+    @validator('sequence')
+    def validate_sequence(cls, v):
+        target.validate_target_sequence(v)
