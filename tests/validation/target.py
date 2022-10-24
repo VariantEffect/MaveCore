3948245982b8af8f22a65b3dbe3cee1f2f2e3480
harmatt <79935163+harmatt@users.noreply.github.com>
2022-10-24 11:52:28 -0700

    add target test cases for valid inputs

--- tests/validation/target.py
+++ tests/validation/target.py
@@ -5,14 +5,19 @@ from mavecore.validation.target import *
 
 class TestValidateTargetCategory(TestCase):
     def test_valid(self):
-        pass
+        for category in valid_categories:
+            validate_target_category(category)
 
 
 class TestValidateSequenceCategory(TestCase):
     def test_valid(self):
-        pass
+        for sequence_type in valid_sequence_types:
+            validate_sequence_category(sequence_type)
 
 
 class TestValidateTargetSequence(TestCase):
+    def setUp(self):
+        self.target_seq = "ATGACCAAACAT"
+
     def test_valid(self):
-        pass
+        validate_target_sequence(self.target_seq)
