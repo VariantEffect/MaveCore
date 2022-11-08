6bd39f433788af25a2846006666fa87b39624b31
harmatt <79935163+harmatt@users.noreply.github.com>
2022-11-08 14:03:11 -0800

    write documentation for scoreset and experiment dictionary validation

--- mavecore/validation/dataset.py
+++ mavecore/validation/dataset.py
@@ -4,6 +4,21 @@ from mavecore.models.data import Experiment, ScoreSet
 
 
 def validate_experiment(experiment: dict):
+    """
+    Validates an experiment represented as a dictionary. Validation is handled via pydantic. A valid dictionary is
+    returned upon validation. If extra or duplicate keys are included, those fields are excluded from the returned
+    dictionary. If required keys are missing or any keys contain incorrect values, an error is raised.
+
+    Parameters
+    __________
+    experiment : dict
+        The experiment dictionary that will be validated.
+
+    Raises
+    ______
+    ValueError
+        If required keys are missing or any keys contain incorrect values.
+    """
     try:
         Experiment.parse_obj(experiment)
     except ValueError as e:
@@ -11,6 +26,22 @@ def validate_experiment(experiment: dict):
 
 
 def validate_scoreset(scoreset: dict):
+    """
+    Validates a scoreset represented as a dictionary (Note: this does not validate dataframes, look to dataframe.py
+    for that validation code). Validation is handled via pydantic. A valid dictionary is returned upon validation.
+    If extra or duplicate keys are included, those fields are excluded from the returned dictionary. If required keys
+    are missing or any keys contain incorrect values, an error is raised.
+
+    Parameters
+    __________
+    experiment : dict
+        The scoreset dictionary that will be validated.
+
+    Raises
+    ______
+    ValueError
+        If required keys are missing or any keys contain incorrect values.
+    """
     try:
         ScoreSet.parse_obj(scoreset)
     except ValueError as e:
